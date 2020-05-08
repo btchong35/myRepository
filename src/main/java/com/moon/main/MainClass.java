a16f3e3ffbf6cfbd6fee929edf8dcf68a99cfb3e
luqing <btchong35@126.com>
2020-05-09 02:37:00 +0800

    idea上传github测试

---
+++ src/main/java/com/moon/main/MainClass.java
@@ -0,0 +1,15 @@
+package com.moon.main;
+
+import java.sql.DriverManager;
+
+/**
+ * 功能描述:<br>
+ * 〈〉
+ *
+ * @author Administrator
+ * @create 2020/5/8
+ * @since 1.0.0
+ */
+public class MainClass {
+    public static void main(String[] args) {
+}}
