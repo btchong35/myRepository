525c3574bfe2c6a1de303c4b93f84d1b29f9c3ec
luqing <btchong35@126.com>
2020-05-13 18:06:33 +0800

    idea上传github测试0

--- src/main/java/com/moon/main/MainClass.java
+++ src/main/java/com/moon/main/MainClass.java
@@ -1,6 +1,12 @@
 package com.moon.main;
 
-import java.sql.DriverManager;
+import com.moon.entity.Product;
+import groovy.transform.ASTTest;
+import org.junit.Test;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 功能描述:<br>
@@ -10,6 +16,30 @@ import java.sql.DriverManager;
  * @create 2020/5/8
  * @since 1.0.0
  */
-public class MainClass {
-    public static void main(String[] args) {
-}}
+public class MainClass{
+
+    @Test
+    public void test() throws ClassNotFoundException, SQLException {
+        List<Product> list=new ArrayList<>();
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/auctionsystem", "root", "123456");
+        PreparedStatement statement=conn.prepareStatement("select * from product");
+        ResultSet res=statement.executeQuery();
+        while(res.next()){
+            Product product=new Product(){
+                @Override
+                public String toString() {
+                    return this.getPid()+"-"+this.getProductname()+"-"+this.getCreatedate();
+                }
+            };
+            product.setPid(res.getInt("pid"));
+            product.setProductname(res.getString("productname"));
+            product.setCreatedate(res.getDate("createdate"));
+            list.add(product);
+        }
+        for(Product e:list){
+            System.out.println(e);
+        }
+
+    }
+}
